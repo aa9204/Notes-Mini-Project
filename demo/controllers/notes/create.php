783c1dd4bf41da68a20827f1a6d5f14581cf6893
aa9204 <aa9204@princeton.edu>
2024-06-26 16:59:27 +0700

    Notes Mini Project

---
+++ demo/controllers/notes/create.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+
+$config = require base_path('config.php');
+
+
+
+$db = new Database($config['database']);
+
+
+
+
+$errors = [];
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+
+
+
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
+
+    }
+
+}
+
+
+
+
+view("notes/create.view.php", [
+
+    'heading' => 'Create Note',
+
+    'errors' => $errors
+
+]);
